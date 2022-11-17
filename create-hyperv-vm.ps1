54c3e4e8a53bb07a87a832a27ae72f6dc06b3087
Onsqa <45150212+Onsqa@users.noreply.github.com>
2022-11-17 20:24:26 +0200

    .iso mounttaus

--- create-hyperv-vm.ps1
+++ create-hyperv-vm.ps1
@@ -1,10 +1,7 @@
-
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
     $VMname = Read-Host "Syötä virtuaalikoneen nimi (esim. Win10-Testi)"
-    # $VMDiskSize = Read-Host "Syötä virtuaalikoneen levytila (GB) (esim. 50)"
-    # $VMRamSize = Read-Host "Syötä virtuaalikoneen RAM-määrä (GB) (esim. 4)"
     $VMISOPath = Read-Host "Syötä virtuaalikoneen asennusmedia (ISO) polku (esim. C:\Windows.iso)"
 
-New-VM -Name $VMname -MemoryStartupBytes 4GB -Path "C:\Hyper-V\Virtual Machines\$VMname" -NewVHDPath "C:\Hyper-V\Virtual Hard Disks\$VMname.vhdx" -NewVHDSizeBytes 30GB -Generation 2 -SwitchName "Default Switch"
-
-
+New-VM -Name $VMname -MemoryStartupBytes 4GB -Path "C:\Hyper-V\Virtual Machines\$VMname" -NewVHDPath "C:\Hyper-V\Virtual Hard Disks\$VMname.vhdx"  -NewVHDSizeBytes 30GB -Generation 2 -SwitchName "Default Switch"
+Add-VMDvdDrive -VMName $VMname -Path $VMISOPath
