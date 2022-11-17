918c1b25e2cf35567afa352f2d6c16408d5e65f6
Onsqa <45150212+Onsqa@users.noreply.github.com>
2022-11-17 20:23:53 +0200

    Add files via upload

---
+++ install-dhcp.ps1
@@ -0,0 +1,31 @@
+#Asennetaan Windows serverille DHCP. Server managerissa pittää completee sit tän jälkeen se securitygroup homma painamal vaa complete, sitä en hokannu
+
+#Alku kyssärit
+$ip = Read-Host "Aseta ip osoite"
+$DefaultGW = Read-Host "Aseta Default Gateway"
+$DNSip = Read-Host "Aseta DNS ip osoite"
+
+$ScopeName = Read-Host "DHCP scopen nimi"
+$StartRange = Read-Host "Aseta startrange scopelle"
+$EndRange = Read-Host "Asaeta endrange scopelle"
+$SubnetMask = Read-Host "Aseta aliverkon maski"
+
+# Asetetaan staattinen ip 
+New-NetIPAddress -IPAddress $ip -DefaultGateway $DefaultGW -PrefixLength 24 -InterfaceIndex (Get-NetAdapter).InterfaceIndex
+Set-DnsClientServerAddress -InterfaceIndex (Get-NetAdapter).InterfaceIndex -ServerAddresses $DNSip
+
+
+# Asennetaan DHCP palvelimelle
+Install-WindowsFeature DHCP -IncludeManagementTools
+#Write-Host("DHCP on asennettu palvelimelle")
+
+# Luodaan DHCP security groupit
+netsh dhcp add securitygroups
+
+
+#Luodaan ja määritetään scopelle asetukset
+Add-DhcpServerv4Scope -name $ScopeName -StartRange $StartRange -EndRange $EndRange -SubnetMask $SubnetMask -State Active
+Set-DhcpServerV4OptionValue -DnsServer $DNSip -Router $DefaultGW
+
+#Restartataan DHCP server
+Restart-Service dhcpserver
